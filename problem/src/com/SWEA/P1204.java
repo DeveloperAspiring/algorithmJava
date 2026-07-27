fcf4b631b841eca1e6e8635b886cdd0e20bf4c6f
DeveloperAspiring <mckim787@gmail.com>
2026-07-27 14:26:00 +0900

    swea 1204번

---
+++ problem/src/com/SWEA/P1204.java
@@ -0,0 +1,38 @@
+package com.SWEA;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P1204 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for(int t = 1; t<= T; t++){
+            int test_case = Integer.parseInt(br.readLine());
+            String input = br.readLine();
+            int [] arr = new int[101];
+            StringTokenizer st = new StringTokenizer(input, " ");
+
+            while (st.hasMoreTokens()) {
+                int number = Integer.parseInt(st.nextToken());
+
+                arr[number] += 1;
+            }
+            int count = 0;
+            int num = 0;
+            for(int i =0 ; i<101 ; i++){
+                if(Integer.compare(count, arr[i]) == -1){
+                    count = arr[i];
+                    num = i;
+                }else if(Integer.compare(count, arr[i]) == 0){
+                    count = arr[i];
+                    num = i;
+                }
+            }
+            System.out.println("#"+ test_case+" "+num);
+        }
+    }
+}
