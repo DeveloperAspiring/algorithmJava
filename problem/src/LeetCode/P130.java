d8f16130c83239eb709091d4e5b96bf3d6956caa
hi <example@email.com>
2026-08-11 16:57:47 +0900

    리트 코드 130번

--- problem/src/LeetCode/P130.java
+++ problem/src/LeetCode/P130.java
@@ -5,46 +5,77 @@ import java.util.Queue;
 
 public class P130 {
     class Solution {
-        private static final int [] dr = {-1,1,0,0};
-        private static final int [] dc = {0,0,-1,1};
+        private static final int[] dr = {-1, 1, 0, 0};
+        private static final int[] dc = {0, 0, -1, 1};
+
         public static void solve(char[][] board) {
-            int size = board.length;
-            for(int row = 0; row < size ; row++){
-                for(int col = 0; col < size ; col++){
-                    if(board[row][col] == 'X') continue;
-                    dfs(board, row,col,size);
+            int rsize = board.length;
+            int csize = board[0].length;
+            for (int row = 0; row < rsize; row++) {
+                for (int col = 0; col < csize; col++) {
+                    if (board[row][col] == 'X') continue;
+                    for (int index = 0; index < 4; index++) {
+                        if (row + dr[index] < 0 || row + dr[index] >= rsize || col + dc[index] < 0 || col + dc[index] >= csize) {
+                            dfs(board,row,col,rsize,csize);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            for (int row = 0; row < rsize; row++) {
+                for (int col = 0; col < csize; col++) {
+                    if(board[row][col] == 'O')board[row][col] ='X';
+                    if(board[row][col] == 'C')board[row][col] ='O';
                 }
             }
 
             System.out.print(Arrays.deepToString(board));
         }
-        public static void dfs(char[][] board, int row, int col, int size){
 
-            board[row][col] =  'C';
-             boolean flag = false;
-            for(int index = 0; index < 4; index++){
-                if(row + dr[index] <0 || row + dr[index] >= size || col + dc[index] <0 || col + dc[index] >= size)continue;
+        public static void dfs(char[][] board, int row, int col, int rsize, int csize) {
 
-                if(board[row + dr[index]][col + dc[index]] == 'O'){
-                    dfs(board, row + dr[index], col + dc[index], size);
-                    flag = true;
+            board[row][col] = 'C';
+            for (int index = 0; index < 4; index++) {
+                if (row + dr[index] < 0 || row + dr[index] >= rsize || col + dc[index] < 0 || col + dc[index] >= csize)continue;
+
+                if (board[row + dr[index]][col + dc[index]] == 'O') {
+                    dfs(board, row + dr[index], col + dc[index], rsize, csize);
+
                 }
-                if(board[row + dr[index]][col + dc[index]] == 'C'){
-                    flag = true;
-                }
-            }
-            if(flag){
-                board[row][col] =  'X';
-            }else{
-                board[row][col] =  'O';
             }
         }
 
 
     }
-    public static void main(String[] args){
-        char[][] board = {{'X','X','X','X'},{'X','O','O','X'},{'X','X','O','X'},{'X','O','X','X'}};
-       // char[][] board = {{'X','X','X','X'},{'X','X','X','X'},{'X','X','X','X'},{'X','O','X','X'}};
-        Solution.solve(board);
+
+    public static void main(String[] args) {
+        char[][] board = {{'X', 'X', 'X', 'X'}, {'X', 'O', 'O', 'X'}, {'X', 'X', 'O', 'X'}, {'X', 'O', 'X', 'X'}};
+        char[][] board1 = {
+                {'X', 'X', 'X', 'X'},
+                {'X', 'X', 'O', 'X'},  // 이중 for문이 여기를 먼저 지나침
+                {'X', 'X', 'O', 'X'},
+                {'X', 'X', 'O', 'X'}   // 테두리
+        };
+        char[][] board2 = {
+                {'X', 'X', 'X', 'X'},
+                {'X', 'O', 'O', 'X'},
+                {'X', 'X', 'X', 'X'}
+        };
+
+        char[][] board3 = {
+                {'O', 'O', 'O'},
+                {'O', 'X', 'O'},
+                {'O', 'O', 'O'}
+        };
+
+        char[][] board4 = {
+                {'O', 'O', 'O', 'X', 'X'},
+                {'X', 'X', 'O', 'X', 'X'},
+                {'X', 'X', 'O', 'O', 'X'},
+                {'X', 'X', 'X', 'X', 'X'}
+        };
+        // char[][] board = {{'X','X','X','X'},{'X','X','X','X'},{'X','X','X','X'},{'X','O','X','X'}};
+        Solution.solve(board4);
     }
 }
