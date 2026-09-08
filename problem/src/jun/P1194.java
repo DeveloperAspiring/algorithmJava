e13ee27ff381ddade2286fa0a4bf91d25a6b8024
jihyun109 <wlguswlgus989@gmail.com>
2026-09-08 17:53:01 +0900

    달이 차오르는

---
+++ problem/src/jun/P1194.java
@@ -0,0 +1,11 @@
+package jun;
+
+//달이 차오르다, 가자
+//아이디어 matrix를 삼중 배열 [row][col][2]만든후 [0]에는 count [1]에는 비트마스킹 형태로 키값을 저장
+//node에서 값을 비교기 현제 키가 포함되어 있는데 cout가 크면 가지치기
+public class P1194 {
+
+    public static void main(String[] args){
+
+    }
+}
